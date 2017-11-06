ea3945084c4dc892e4d39607234a5be9a44503c5
Yota Toyama <raviqqe@gmail.com>
2017-11-07 03:40:21 +0900

    Use SSL

--- main.tf
+++ main.tf
@@ -1,15 +1,66 @@
 variable "domain" {}
 
-variable "ttl" {
-  default = 3600
+variable "certificate" {
+  default = "arn:aws:acm:us-east-1:515821172331:certificate/3160c15b-1e40-4eb1-bab6-61efe5ddc73c"
 }
 
-variable "ip_addresses" {
-  default = ["192.30.252.153", "192.30.252.154"]
+variable "origin" {
+  default = "raviqqe.github.io"
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_cloudfront_distribution" "d" {
+  aliases = ["${var.domain}", "www.${var.domain}"]
+
+  origin {
+    domain_name = "${var.origin}"
+    origin_id   = "${var.origin}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.origin}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${var.certificate}"
+    ssl_support_method  = "sni-only"
+  }
 }
 
 resource "aws_route53_zone" "z" {
@@ -19,17 +70,25 @@ resource "aws_route53_zone" "z" {
 resource "aws_route53_record" "root" {
   zone_id = "${aws_route53_zone.z.zone_id}"
   name    = "${var.domain}"
-  ttl     = "${var.ttl}"
   type    = "A"
-  records = "${var.ip_addresses}"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.d.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.d.hosted_zone_id}"
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_route53_record" "www" {
   zone_id = "${aws_route53_zone.z.zone_id}"
   name    = "www.${var.domain}"
-  ttl     = "${var.ttl}"
   type    = "A"
-  records = "${var.ip_addresses}"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.d.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.d.hosted_zone_id}"
+    evaluate_target_health = false
+  }
 }
 
 output "name_servers" {
