0ca3a379903ebc487fdb76cc0054dabeb73a7487
Yota Toyama <raviqqe@gmail.com>
2017-11-07 13:29:28 +0900

    Build favicon

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,33 @@
+require 'xml-dsl'
+
 DOMAIN = 'raviqqe.com'.freeze
 
-task :default do
+def points(*points)
+  points.map do |x, y|
+    "#{x},#{y}"
+  end.join ' '
+end
+
+file 'favicon.png' do |t|
+  full = 4242
+
+  source = xml do
+    svg xmlns: 'http://www.w3.org/2000/svg', width: full, height: full do
+      rect x: 0, y: 0, width: full, height: full, fill: :white
+      polygon points: points([0, 0], [full, 0], [full, full]), fill: :red
+      polygon points: points([0, 0], [0, full], [full, 0]), fill: :black
+    end
+  end
+
+  File.write 'favicon.svg', source
+
+  sh %W[inkscape
+        --export-width 16 --export-height 16
+        --export-png #{t.name}
+        favicon.svg].join ' '
+end
+
+task default: 'favicon.png' do
   sh 'terraform init'
   sh "terraform apply -auto-approve -var domain=#{DOMAIN}"
 
