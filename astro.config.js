b09e30bffb87a31be83c2ba524ba900e95d82ba2
Yota Toyama <raviqqe@gmail.com>
2023-06-28 17:21:31 +1000

    Configure sitemap (#29)

--- astro.config.js
+++ astro.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({ site: "https://raviqqe.com" });
+export default defineConfig({
+  site: "https://raviqqe.com",
+  integrations: [sitemap()],
+});
