550056c797b9a04ddaa2b2028d4926b7bd91a319
Yota Toyama <raviqqe@gmail.com>
2023-09-01 23:11:49 +0930

    chore/image (#65)
    - Fix typo (#60)
    - Use `.ts` configuration file (#61)
    - Bump @astrojs/sitemap from 2.0.2 to 3.0.0 (#62)
    - Bump astro from 2.10.15 to 3.0.5 (#64)
    - Add image configuration

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,9 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://raviqqe.com",
+  image: {
+    service: { entrypoint: "astro/assets/services/sharp" },
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [mdx(), sitemap()],
 });
