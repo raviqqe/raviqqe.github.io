387987e4aa3040ca87ac72b164ecd017d36668dd
Yota Toyama <raviqqe@gmail.com>
2023-09-09 21:53:23 +1000

    Revert "Fix"
    This reverts commit d9a4a44c9ed570bd07cca09c80914824a4d99219.

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://raviqqe.com",
   image: {
     service: { entrypoint: "astro/assets/services/sharp" },
     remotePatterns: [{ protocol: "https" }],
