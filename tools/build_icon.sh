61914e74f2d6ba2c9827e49ce0ddac93409f826b
Yota Toyama <raviqqe@gmail.com>
2024-08-19 23:41:08 +1000

    Icons (#273)
    - **Fix**
    - **Fix**
    - **Fix**
    - **Fix**

---
+++ tools/build_icon.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 0 ]; then
+  exit 1
+fi
+
+cd $(dirname $0)/..
+
+for size in 16 32 48 64 128 256 512 1024; do
+  inkscape --batch-process --export-filename public/icon-$size.png --export-width $size public/icon.svg
+done
