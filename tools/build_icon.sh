1c56e56e570617b1094e5f93e53c5a0b46f2554b
Yota Toyama <raviqqe@gmail.com>
2024-08-19 22:14:53 +0900

    Fix

--- tools/build_icon.sh
+++ tools/build_icon.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ $# -ne 0 ]; then
+  exit 1
+fi
+
 cd $(dirname $0)/..
 
-inkscape --batch-process --export-filename public/$2 --export-width $1 public/icon.svg
+for size in 16 32 48 64 128 256 512; do
+  inkscape --batch-process --export-filename public/icon-$size.png --export-width $size public/icon.svg
+done
