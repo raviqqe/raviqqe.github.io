37bd9c7f0c45d47a46354a7758ef7c1855c56b82
Yota Toyama <raviqqe@gmail.com>
2025-04-02 23:18:59 -0700

    Disable image service

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://raviqqe.com",
-  image: {
-    service: sharpImageService(),
-    remotePatterns: [{ protocol: "https" }],
-  },
   integrations: [mdx(), prefetch({ intentSelector: ["a"] }), sitemap()],
 });
