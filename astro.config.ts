5b963acd71cc6dc09d6b800641141583c479776a
Yota Toyama <raviqqe@gmail.com>
2025-12-24 00:04:52 -0800

    Fix

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,9 @@
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  image: {
-    remotePatterns: [{ protocol: "https" }],
-    service: passthroughImageService(),
-  },
   integrations: [mdx(), prefetch({ intentSelector: ["a"] }), sitemap()],
   site: "https://raviqqe.com",
 });
