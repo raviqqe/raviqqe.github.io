20120b01a993dc8838bccfda5b6f5970557f05a0
Yota Toyama <raviqqe@gmail.com>
2025-12-24 00:29:56 -0800

    Refactor (#492)

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://raviqqe.com",
   integrations: [mdx(), prefetch({ intentSelector: ["a"] }), sitemap()],
+  site: "https://raviqqe.com",
 });
