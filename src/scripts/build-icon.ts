2862b00ae4bfdd6cc46239a41a1362bb26b380c1
Yota Toyama <raviqqe@gmail.com>
2026-08-25 22:36:31 -0700

    Refactor (#620)

--- src/scripts/build-icon.ts
+++ src/scripts/build-icon.ts
@@ -2,12 +2,14 @@ import { readFile, writeFile } from "node:fs/promises";
 import { renderAsync } from "@resvg/resvg-js";
 import { iconSizes } from "../icon-sizes.ts";
 
-const svg = await readFile(new URL("../../public/icon.svg", import.meta.url));
+const svg = await readFile(
+  new URL(import.meta.resolve("../../public/icon.svg")),
+);
 
 await Promise.all(
   iconSizes.map(async (size) =>
     writeFile(
-      new URL(`../../public/icon-${size}.png`, import.meta.url),
+      new URL(import.meta.resolve(`../../public/icon-${size}.png`)),
       (
         await renderAsync(svg, { fitTo: { mode: "width", value: size } })
       ).asPng(),
